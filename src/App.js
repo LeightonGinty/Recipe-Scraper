c166d28132d062703baed15afcd3a7623cdd3a2e
LeightonGinty <57670831+LeightonGinty@users.noreply.github.com>
2022-08-26 20:15:11 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import $ from 'jquery'
+import SearchBar from './SearchBar.jsx';
+import React from 'react';
+
+// var request = require("request");
+// var cheerio = require("cheerio");
+function App() {  
+
+  return (
+    <div className="App">
+       
+      <body>
+
+      <SearchBar>
+
+      </SearchBar>
+
+
+
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/getArticles.js
@@ -0,0 +1,6 @@
+const cheerio = require('cheerio');
+import axios from 'axios';
+  const axios = require('axios').default;
+
+
+var url = "https://www.birmingham.ac.uk/index.aspx"
